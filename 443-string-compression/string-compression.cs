aadc3b6477a4adb8aa047e608fb87dd1a77e62e7
Faruk Esen <farukesen39@gmail.com>
2025-12-03 10:05:19 +0300

    Time: 5 ms (47.47%) | Memory: 63 MB (25.13%) - LeetSync

---
+++ 443-string-compression/string-compression.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int Compress(char[] chars) {
+        int write = 0;  
+        int read = 0;   
+        
+        while(read < chars.Length) {
+            char currentChar = chars[read];  
+            int count = 0;  
+
+            while(read < chars.Length && chars[read] == currentChar) {
+                read++;
+                count++;
+            }
+            
+            chars[write] = currentChar;
+            write++;
+            
+            if(count > 1) {
+                string countStr = count.ToString();
+                foreach(char c in countStr) {
+                    chars[write] = c;
+                    write++;
+                }
+            }
+        }
+        
+        return write;
+    }
+    
+}
