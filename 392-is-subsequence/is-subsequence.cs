47794aaeaee888973cc8aa40483ca8c89c466737
Faruk Esen <farukesen39@gmail.com>
2025-12-10 22:53:16 +0300

    Time: 0 ms (100.00%) | Memory: 41.3 MB (93.03%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        if (s.Length == 0) return true;
+        
+        int i = 0;
+        for (int j = 0; j < t.Length; j++) {
+            
+            if (s[i] == t[j]) {
+                i++; 
+                
+             
+                if (i == s.Length) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
